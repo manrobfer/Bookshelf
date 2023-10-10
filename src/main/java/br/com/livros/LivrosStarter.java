1e9080afb644b4575f77a00b62c42f9f60d2134d
Manoel <manoell@hotmail.com>
2023-10-10 18:32:15 -0300

    Log no App

--- src/main/java/br/com/livros/LivrosStarter.java
+++ src/main/java/br/com/livros/LivrosStarter.java
@@ -1,12 +1,15 @@
 package br.com.livros;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
-public class LivrosStarter {	
+public class LivrosStarter {
+	static Logger log = Logger.getLogger(LivrosStarter.class.getName());
 	
 	public static void main(String[] args) {
 		
